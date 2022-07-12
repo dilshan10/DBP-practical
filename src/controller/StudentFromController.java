e706cb8a6392302628bee57f8058ef1961ae26c8
dilshan10 <superdilshan999@gmail.com>
2022-07-12 11:15:06 +0530

    Add Save And Update

--- src/controller/StudentFromController.java
+++ src/controller/StudentFromController.java
@@ -1,9 +1,14 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import model.Student;
+import util.CrudUtil;
+
+import java.sql.SQLException;
 
 public class StudentFromController {
     public TextField txtSearch;
@@ -20,14 +25,54 @@ public class StudentFromController {
     public TableColumn tblContact;
     public TableColumn tblAddress;
     public TableColumn tblNic;
+    public TextField txtContact;
 
     public void UpdateOnAction(ActionEvent actionEvent) {
+        Student student =new Student(
+                txtId.getText(),
+                txtName.getText(),
+                txtEmail.getText(),
+                txtContact.getText(),
+                txtAddress.getText(),
+                txtNic.getText()
+        );
+        try {
+            if (CrudUtil.execute("UPDATE student SET student_name=?,email=?,contact=?,address=?,nic=? WHERE student_id=?", student.getStudent_name(),student.getEmail(),student.getContact(),student.getAddress(),student.getNic(),student.getStudent_id())){
+                new Alert(Alert.AlertType.CONFIRMATION,"Updated!").show();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Error").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveOnAction(ActionEvent actionEvent) {
+        Student student=new Student(
+                txtId.getText(),
+                txtName.getText(),
+                txtEmail.getText(),
+                txtContact.getText(),
+                txtAddress.getText(),
+                txtNic.getText()
+        );
+        try {
+            if(CrudUtil.execute("INSERT INTO student VALUES (?,?,?,?,?,?)", student.getStudent_id(),student.getStudent_name(),student.getEmail(),student.getContact(),student.getAddress(),student.getNic())){
+                new Alert(Alert.AlertType.CONFIRMATION,"Student Saved!").show();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Error").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void DeleteOnAction(ActionEvent actionEvent) {
+
     }
 
     public void serachOnAction(ActionEvent actionEvent) {
--- src/util/CrudUtil.java
+++ src/util/CrudUtil.java
@@ -5,7 +5,7 @@ import db.DBConnection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class UrudUtil {
+public class CrudUtil {
     public static <T> T execute(String sql,Object...params) throws SQLException, ClassNotFoundException {
         PreparedStatement statement= DBConnection.getInstance().getConnection().prepareStatement(sql);
         for (int i=0;i<params.length;i++){
