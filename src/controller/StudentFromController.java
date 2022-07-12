29e8e46f78ba8eaa2abf64506442a1bf0a1d4f8f
dilshan10 <superdilshan999@gmail.com>
2022-07-12 11:47:52 +0530

    Add Serach Student And Load All Student

--- src/controller/StudentFromController.java
+++ src/controller/StudentFromController.java
@@ -1,13 +1,17 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Student;
 import util.CrudUtil;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class StudentFromController {
@@ -18,7 +22,7 @@ public class StudentFromController {
     public TextField ttContact;
     public TextField txtAddress;
     public TextField txtNic;
-    public TableView tblStudent;
+    public TableView<Student> tblStudent;
     public TableColumn tblStudentId;
     public TableColumn tblStudentName;
     public TableColumn tblEmail;
@@ -26,6 +30,23 @@ public class StudentFromController {
     public TableColumn tblAddress;
     public TableColumn tblNic;
     public TextField txtContact;
+
+    public void initialize(){
+        tblStudentId.setCellValueFactory(new PropertyValueFactory("student_id"));
+        tblStudentName.setCellValueFactory(new PropertyValueFactory("student_name"));
+        tblEmail.setCellValueFactory(new PropertyValueFactory("email"));
+        tblContact.setCellValueFactory(new PropertyValueFactory("contact"));
+        tblAddress.setCellValueFactory(new PropertyValueFactory("address"));
+        tblNic.setCellValueFactory(new PropertyValueFactory("nic"));
+
+        try {
+            LoadAllStudent();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void UpdateOnAction(ActionEvent actionEvent) {
         Student student =new Student(
@@ -72,9 +93,52 @@ public class StudentFromController {
     }
 
     public void DeleteOnAction(ActionEvent actionEvent) {
-
+        try {
+            if (CrudUtil.execute("DELETE FROM student WHERE student_id=?", txtSearch.getText())){
+                new Alert(Alert.AlertType.CONFIRMATION,"Deleted").show();
+            }else{
+                new Alert(Alert.AlertType.ERROR,"Error").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void serachOnAction(ActionEvent actionEvent) {
+        try {
+            ResultSet rst= CrudUtil.execute("SELECT * FROM student WHERE student_id=?", txtSearch.getText());
+            if (rst.next()) {
+                txtId.setText(rst.getString(1));
+                txtName.setText(rst.getString(2));
+                txtEmail.setText(rst.getString(3));
+                txtContact.setText(rst.getString(4));
+                txtAddress.setText(rst.getString(5));
+                txtNic.setText(rst.getString(6));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public void LoadAllStudent() throws SQLException, ClassNotFoundException {
+            ResultSet rst = CrudUtil.execute("SELECT * FROM student"  );
+        System.out.println(rst);
+            ObservableList<Student> oblist= FXCollections.observableArrayList();
+            while (rst.next()){
+                oblist.add(
+                        new Student(
+                                rst.getString(1),
+                                rst.getString(2),
+                                rst.getString(3),
+                                rst.getString(4),
+                                rst.getString(5),
+                                rst.getString(6)
+                        )
+                );
+            }
+            tblStudent.setItems(oblist);
     }
 }
