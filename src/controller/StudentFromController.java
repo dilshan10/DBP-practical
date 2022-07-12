efc3bee952452418c0e17ce6108d544451d539fc
dilshan10 <superdilshan999@gmail.com>
2022-07-12 10:52:30 +0530

    Add ui and CrudUtil

---
+++ src/controller/StudentFromController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentFromController {
+    public TextField txtSearch;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField ttContact;
+    public TextField txtAddress;
+    public TextField txtNic;
+    public TableView tblStudent;
+    public TableColumn tblStudentId;
+    public TableColumn tblStudentName;
+    public TableColumn tblEmail;
+    public TableColumn tblContact;
+    public TableColumn tblAddress;
+    public TableColumn tblNic;
+
+    public void UpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void serachOnAction(ActionEvent actionEvent) {
+    }
+}
